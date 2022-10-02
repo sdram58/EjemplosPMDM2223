071470c36fd4ca3b2474a4fdab6f896a38683a53
sdram58 <sdram58@hotmail.com>
2022-10-02 19:06:11 +0200

    Click counter finished

--- UD3/ClicksCounter/app/src/main/java/com/catata/clickscounter/MainActivity.kt
+++ UD3/ClicksCounter/app/src/main/java/com/catata/clickscounter/MainActivity.kt
@@ -2,10 +2,21 @@ package com.catata.clickscounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.catata.clickscounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var counter = 0  //creates a initial counter variable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(ActivityMainBinding.inflate(layoutInflater).also { binding = it }.root)
+
+        binding.increaseCounter.setOnClickListener {
+            binding.clicksCounter.text = "You have clicked ${++counter} times"
+        }
     }
 }

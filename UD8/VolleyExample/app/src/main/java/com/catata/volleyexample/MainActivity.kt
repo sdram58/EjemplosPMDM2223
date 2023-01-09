3c5e3ed4276d2e68480cf467e90a7c9962381dd6
sdram58 <sdram58@hotmail.com>
2023-01-09 14:26:38 +0100

    Volley Example

---
+++ UD8/VolleyExample/app/src/main/java/com/catata/volleyexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.catata.volleyexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.catata.volleyexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    
+        setContentView(ActivityMainBinding.inflate(layoutInflater).also { binding = it }.root)
+        
+        
+    }
+}

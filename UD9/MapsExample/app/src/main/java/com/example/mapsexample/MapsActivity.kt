388a7b126a51d9ffb7187a384639d3b5f5ed5e05
sdram58 <sdram58@hotmail.com>
2023-01-27 12:01:58 +0100

    Maps updated

--- UD9/MapsExample/app/src/main/java/com/example/mapsexample/MapsActivity.kt
+++ UD9/MapsExample/app/src/main/java/com/example/mapsexample/MapsActivity.kt
@@ -1,8 +1,13 @@
 package com.example.mapsexample
 
+import android.content.pm.PackageManager
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,13 +16,23 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.mapsexample.databinding.ActivityMapsBinding
+import com.google.android.gms.location.*
+import com.google.android.gms.location.Priority.PRIORITY_BALANCED_POWER_ACCURACY
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.Marker
 
+
+const val REQUEST_PERMISSION_CODE=110
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private var mLatitude = 0.0
+    private var mLongitude = 0.0
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +46,52 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnInfoWi
         mapFragment.getMapAsync(this)
 
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        //This new Google location services only works with API 31 and greater
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S_V2) {
+
+            val locationRequestBuilder = LocationRequest.Builder(16000)
+            locationRequestBuilder.setDurationMillis(Long.MAX_VALUE)
+            locationRequestBuilder.setPriority(PRIORITY_BALANCED_POWER_ACCURACY)
+            locationRequestBuilder.setMinUpdateDistanceMeters(0f)
+            locationRequest = locationRequestBuilder.build()
+        }
+
+        locationCallback = object: LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult){
+                super.onLocationResult(locationResult)
+               for(location in locationResult.locations){
+                    mLatitude = location.latitude
+                    mLongitude = location.longitude
+                    /*Toast.makeText(this@MapsActivity, "Lat:" + mLatitude + "Lon:" +
+                            mLongitude, Toast.LENGTH_LONG) .show();*/
+
+                   Log.d("LOCATION_MSG", "($mLatitude, $mLongitude)")
+                }
+            }
+        }
+
+        requestLocations()
+    }
+
+    private fun requestLocations() {
+        if (ActivityCompat.checkSelfPermission(this@MapsActivity,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this@MapsActivity,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this@MapsActivity,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                REQUEST_PERMISSION_CODE)
+        } else {
+            mFusedLocationClient.requestLocationUpdates(
+                locationRequest, locationCallback, Looper.getMainLooper())
+        }
+    }
+
+    private fun removeLocations() {
+        mFusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
     /**

7951470845f7e526c6cc32d5cdffc6c714cc7702
sdram58 <sdram58@hotmail.com>
2023-01-30 12:41:42 +0100

    Notifications Example

--- UD10/NotificationsExample/app/src/main/java/com/example/notificationsexample/YesActivity.kt
+++ UD10/NotificationsExample/app/src/main/java/com/example/notificationsexample/YesActivity.kt
@@ -2,10 +2,18 @@ package com.example.notificationsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notificationsexample.databinding.ActivityYesBinding
 
 class YesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityYesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_yes)
+
+        setContentView(ActivityYesBinding.inflate(layoutInflater).also { binding = it }.root)
+
+        intent.extras?.getString(YES_EXTRA_KEY)?.let {
+            binding.tvText.text = it
+        }
     }
 }
